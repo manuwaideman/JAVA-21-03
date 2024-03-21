43f97b5b8500599d0d49537c067e193be51e026c
Manoella Waideman <mherreriaswaideman@gmail.com>
2024-03-21 16:31:24 -0300

    Create UsaFuncionario1.java

---
+++ UsaFuncionario1.java
@@ -0,0 +1,13 @@
+package br.com.fiap;
+
+public class UsaFuncionario1 {
+  public static void main(String[] args) {
+    Funcionario funcionario1 = new Funcionario();
+
+    funcionario1.setNome("Astrogildo");
+    funcionario1.setValorHora(15.17f);
+
+    //Este funcionário trabalha 20h/semana
+    System.out.print1n
+  }
+}
